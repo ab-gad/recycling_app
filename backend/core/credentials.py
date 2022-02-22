973f67454bfb6a750954ebecc652fd5c6c051aa8
ab-gad <philosopher.gad@gmail.com>
2022-02-22 23:04:56 +0200

    DB settings added & linked to postgres

---
+++ backend/core/credentials.py
@@ -0,0 +1,18 @@
+# Database
+# DB_PASSWORD = '123456'
+# DB_HOST = 'localhost'
+# DB_USER = 'postgres'
+
+DB_PASSWORD = '0000'
+DB_HOST = 'localhost'
+DB_USER = 'team4'
+#Social Auth
+SOCIAL_AUTH_FACEBOOK_KEY = ''    # App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = ''  # App Secret
+
+
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
+
+
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from .credentials import *
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'recycling',
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
