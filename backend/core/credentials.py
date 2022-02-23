03a6ffd3f678df0cd5b7a1773111939426787d15
Adham Atef <adham.atef45@gmail.com>
2022-02-24 01:49:15 +0200

    solving conflict

--- backend/core/credentials.py
+++ backend/core/credentials.py
@@ -3,9 +3,9 @@
 # DB_HOST = 'localhost'
 # DB_USER = 'postgres'
 
-DB_PASSWORD = 'adham8585'
+DB_PASSWORD = '0000'
 DB_HOST = 'localhost'
-DB_USER = 'postgres'
+DB_USER = 'team4'
 # Social Auth
 SOCIAL_AUTH_FACEBOOK_KEY = ''    # App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = ''  # App Secret
