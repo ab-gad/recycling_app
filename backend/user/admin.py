56acc33f3e4983ebc87ea12f1ace00ce6dc2d1af
ab-gad <philosopher.gad@gmail.com>
2022-02-23 23:25:18 +0200

    testing restapi custom perms

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -3,8 +3,10 @@ from django.contrib.gis.admin import OSMGeoAdmin
 
 # Register your models here.
 from .models import User, userLocation
+from events.models import Events
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Events)
 
 @admin.register(userLocation)
 class ShopAdmin(OSMGeoAdmin):
--- backend/user_api/urls.py
+++ backend/user_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserList,LocationList
+from .views import UserList,LocationList, EventDetails
 
 app_name = 'user_api'
 
 urlpatterns = [
     path('', UserList.as_view(), name='userlist'),
-    path('location', LocationList.as_view(), name='locationlist')
+    path('location/', LocationList.as_view(), name='locationlist'),
+    path('event/<int:pk>/', EventDetails.as_view(), name='eventdetails'),
     
 ]
