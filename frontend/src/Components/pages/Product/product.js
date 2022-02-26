2a2d57d13dc498ca822c28816d646ddcc6f69275
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2022-02-26 02:47:03 +0200

    adding product home

---
+++ frontend/src/Components/pages/Product/product.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Homeproduct from "./homeproduct"; 
+
+// const ProductDisplay = () => (
+
+// );
+
+
+// export default function Product() {
+
+
+
+
+// }
