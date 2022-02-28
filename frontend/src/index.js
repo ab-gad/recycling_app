94479390fce9bcb50b0562455de0e8134cffdf01
ab-gad <philosopher.gad@gmail.com>
2022-02-28 16:20:38 +0200

    fix redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from "react-redux";
 import App from './App';
-import {configureStore}from"@reduxjs/toolkit";
-import productsReducer ,{productsFetch}from "./features/productsSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import productsReducer, { productsFetch } from "./features/productsSlice";
 import { productsApi } from './features/productsApi';
-import cartReducer,{getTotals} from './features/cartSlice';
-const store = configureStore({
-    reducer:{
-        products:productsReducer,
-        cart:cartReducer,
-        [productsApi.reducerPath]:productsApi.reducer,
+import cartReducer, { getTotals } from './features/cartSlice';
 
-    },
-    middleware: (getDefaultMiddleware) =>{
-    return getDefaultMiddleware().concat(productsApi.middleware);
-    },
-    
-    // cartReducer,composeWithDevTools()
-});
+//Redux
+import {Provider} from 'react-redux'
+import store from "./redux/store";
+
+// const store = configureStore({
+//   // reducer:{
+//   //     products:productsReducer,
+//   //     cart:cartReducer,
+//   //     [productsApi.reducerPath]:productsApi.reducer,
+
+//   // },
+//   middleware: (getDefaultMiddleware) => {
+//     return getDefaultMiddleware().concat(productsApi.middleware);
+//   },
+
+//   // cartReducer,composeWithDevTools()
+// });
 store.dispatch(getTotals());
 store.dispatch(productsFetch());
 
 ReactDOM.render(
-  <Provider store= {store}>
+  <Provider store={store}>
 
     <App />
-    </Provider>,
+  </Provider>,
   document.getElementById('root')
 );
