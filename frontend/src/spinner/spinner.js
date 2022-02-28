b7db21e05365d414705d908aa2f56f09f9a47723
691997 <95089880+691997@users.noreply.github.com>
2022-02-28 09:03:06 +0200

    create load spinner

---
+++ frontend/src/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React , { useEffect } from 'react';
+import { RiRecycleLine } from "react-icons/ri";
+import './spinner.css';
+
+function Spinner() {
+
+    useEffect( () => {
+        let spinner = document.getElementById("load_animation")
+        window.addEventListener("load" , function(){
+            spinner.style.display = "none"
+        })
+    })
+
+  return (
+    <div id="load_animation" >
+      <RiRecycleLine className="animation" />
+    </div>
+  );
+}
+
+export default Spinner;
