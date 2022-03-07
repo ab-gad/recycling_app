e2c2f67e4292a7544dfaf7297cbaf7d18b067344
Ahmed Nabil <94ahmed.nabil@gmail.com>
2022-03-08 01:18:10 +0200

    rating edit

--- frontend/src/Components/pages/Home/Rateing/rateing.js
+++ frontend/src/Components/pages/Home/Rateing/rateing.js
@@ -33,13 +33,19 @@ function Rateing (){
 
   const { langcont, Setlangcontext } = useContext(Langcontext);
   const translation = langcont === "ENGLISH" ? English : Arabic;
+  
+  const [paperRate, setPaperRate] = useState({})
+  const [plasticRate, setPlasticRate] = useState({})
+  const [aluminuimRate, setAluminuimRate] = useState({})
 
-  const [rate, setRate] = useState([])
    const getRate=() => {
             axios.get(`http://localhost:8000/material_api/rate/`)
             .then((result) => {
-                console.log('rate',result.data)
-                setRate(result.data)
+                console.log('rate',result.data.filter((e)=>e.id == 3)[0])
+                setPaperRate(result.data.filter((e)=>e.id == 1)[0])
+                setPlasticRate(result.data.filter((e)=>e.id == 2)[0])
+                setAluminuimRate(result.data.filter((e)=>e.id == 3)[0])
+                
             })
             .catch((err) => {
                 console.log(err)
@@ -52,12 +58,9 @@ function Rateing (){
       
   },[])
 
-  // console.log('material',rate[2].annual_production)
-  // console.log('material',material[0].annual_production)
 
     return (
-
-      <section id="reat_section" className=" my-5 p-3 fs-sm-6 fs-md-1 text-light">
+        <section id="reat_section" className=" my-5 p-3 fs-sm-6 fs-md-1 text-light">
         <table className="table table-borderless text-center">
           <thead className="">
             <tr className=" row align-items-center">
@@ -69,7 +72,7 @@ function Rateing (){
                 {translation.annual} <br /> {translation.consumption}
               </th>
               <th className="col-4 col-sm-3">
-                 {translation.annual} <br /> {translation.recycling} 
+                {translation.annual} <br /> {translation.recycling} 
               </th>
             </tr>
           </thead>
@@ -77,16 +80,16 @@ function Rateing (){
             <tr className=" row justify-content-between align-items-centermt-md-2">
               <td className="col-12 col-sm-3 mb-3 mb-sm-0"> {translation.papers} </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[1]?.annual_production} />
-                 <span>{rate[1]?.annual_production}%</span>
+                <Progress value={paperRate?.annual_production} />
+                <span>{paperRate?.annual_production}%</span>
               </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[1]?.annual_consumption}  />
-                <span>{rate[1]?.annual_consumption}%</span>
+                <Progress value={paperRate?.annual_consumption}  />
+                <span>{paperRate?.annual_consumption}%</span>
               </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[1]?.annual_recycling} />
-                <span>{rate[1]?.annual_recycling}%</span>
+                <Progress value={paperRate?.annual_recycling} />
+                <span>{paperRate?.annual_recycling}%</span>
               </td>
             </tr>
             <tr className=" row justify-content-between align-items-center mt-5 mt-md-2">
@@ -94,31 +97,31 @@ function Rateing (){
               {translation.plastic}
               </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[0]?.annual_production}  />
-                <span>{rate[0]?.annual_production} %</span>
+                <Progress value={plasticRate?.annual_production}  />
+                <span>{plasticRate?.annual_production} %</span>
               </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[0]?.annual_consumption}  />
-                <span>{rate[0]?.annual_consumption} %</span>
+                <Progress value={plasticRate?.annual_consumption}  />
+                <span>{plasticRate?.annual_consumption} %</span>
               </td>
               <td className="col-4 col-sm-3">
-                <Progress value= {rate[0]?.annual_recycling}/>
-                <span>{rate[0]?.annual_recycling}%</span>
+                <Progress value= {plasticRate?.annual_recycling}/>
+                <span>{plasticRate?.annual_recycling}%</span>
               </td>
             </tr>
             <tr className=" row justify-content-between align-items-center mt-5 mt-md-2">
               <td className="col-12 col-sm-3 mb-3 mb-sm-0"> {translation.Aluminum} </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[2]?.annual_production} />
-                <span>{rate[2]?.annual_production}%</span>
+                <Progress value={aluminuimRate?.annual_production} />
+                <span>{aluminuimRate?.annual_production}%</span>
               </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[2]?.annual_consumption} />
-                <span>{rate[2]?.annual_consumption}%</span>
+                <Progress value={aluminuimRate?.annual_consumption} />
+                <span>{aluminuimRate?.annual_consumption}%</span>
               </td>
               <td className="col-4 col-sm-3">
-                <Progress value={rate[2]?.annual_recycling} />
-                <span>{rate[2]?.annual_recycling}%</span>
+                <Progress value={aluminuimRate?.annual_recycling} />
+                <span>{aluminuimRate?.annual_recycling}%</span>
               </td>
             </tr>
           </tbody>
@@ -135,7 +138,7 @@ function Rateing (){
             </tr>
           </tfoot>
         </table>
-      </section> 
+        </section> 
 
     );
 } 
