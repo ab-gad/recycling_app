44485f209ed858859d5225131e76018e50adb627
Ahmed Nabil <94ahmed.nabil@gmail.com>
2022-03-09 02:00:21 +0200

    event tabs style edit

--- frontend/src/Components/pages/user/events.js
+++ frontend/src/Components/pages/user/events.js
@@ -20,18 +20,23 @@ const Events = () => {
     return (
         <section id="events_container">
             <h1 className="text-center"> {`Welcome ${authed_user && authed_user.first_name} ${authed_user && authed_user.last_name}`} </h1>
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="volunteer-tab" data-bs-toggle="tab" data-bs-target="#volunteer" type="button" role="tab" aria-controls="volunteer" aria-selected="true">Volunteer</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="interested-tab" data-bs-toggle="tab" data-bs-target="#interested" type="button" role="tab" aria-controls="interested" aria-selected="false">Interested</button>
-                </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="volunteer" role="tabpanel" aria-labelledby="volunteer-tab"><Volunteer/> </div>
-                <div class="tab-pane fade" id="interested" role="tabpanel" aria-labelledby="interested-tab"><Interest/></div>
-            </div>
+                <div className="tabs_container">
+                    <ul className="nav nav-pills d-flex justify-content-center mt-4" id="pills-tab" role="tablist " >
+                        <li className="nav-item me-2  " role="presentation">
+                            <button className="nav-link active" id="pills-volunteer-tab" data-bs-toggle="pill" data-bs-target="#pills-volunteer" type="button" role="tab" aria-controls="pills-volunteer" aria-selected="true">Volunteer</button>
+                        </li>
+                        <li className="nav-item ms-2  " role="presentation">
+                            <button className="nav-link" id="pills-interested-tab" data-bs-toggle="pill" data-bs-target="#pills-interested" type="button" role="tab" aria-controls="pills-interested" aria-selected="false">Interested</button>
+                        </li>
+                    </ul>
+                    <div className="tab-content " id="myTabContent">
+                        <div className="tab-pane fade show active" id="pills-volunteer" role="tabpanel" aria-labelledby="pills-volunteer-tab"><Volunteer/> </div>
+                        <div className="tab-pane fade" id="pills-interested" role="tabpanel" aria-labelledby="pills-interested-tab"><Interest/></div>
+                    </div>
+                </div>
+
+            
+
         </section>
 
         
