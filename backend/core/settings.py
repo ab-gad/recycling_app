cb05bdd179f422e440fcf6d9c9a7d6bf436b3494
Adham Atef <adham.atef45@gmail.com>
2022-03-14 16:18:58 +0200

    Final Touches

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,7 +13,7 @@ from .credentials import *
 from pathlib import Path
 import os
 from datetime import timedelta
-
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
